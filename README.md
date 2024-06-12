15d384d672c10b29e8f06e04cb268cea5c39d0b2
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-06-12 09:31:09 -0700

    initial commit of code from nushell issue 10763

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# pipstream
+
+cr = cargo run
+
+```rust
+cr out> /dev/null
+cr out > /dev/null
+```
+
+### References
+
+- [Piping is extremely slow](https://github.com/nushell/nushell/issues/10763)
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::io::{stdout, Write};
+use std::time::{Duration, Instant};
+
+const BUF_SIZE: usize = 8 * 1024;
+const WRITE_CNT: usize = 8 * 1024;
+const BYTES_LEN: usize = BUF_SIZE * WRITE_CNT;
+const SAMPLES: usize = 15;
+
+const BUF: [u8; BUF_SIZE] = [0xFF; BUF_SIZE];
+
+fn main() {
+    let mut stdout = stdout().lock();
+
+    let mut samples: Vec<_> = (0..SAMPLES).map(|_| run(&mut stdout)).collect();
+    samples.sort();
+
+    let min = samples[0];
+    let max = samples[SAMPLES - 1];
+    let med = samples[SAMPLES / 2];
+
+    eprintln!("Elapsed: [{min:?}, {med:?}, {max:?}]");
+    eprintln!(
+        "Throughput: [{:.2} MB/s, {:.2} MB/s, {:.2} MB/s]",
+        thpt(min),
+        thpt(med),
+        thpt(max)
+    );
+}
+
+fn run<W: Write>(out: &mut W) -> Duration {
+    let start = Instant::now();
+    for _ in 0..WRITE_CNT {
+        out.write_all(&BUF).unwrap();
+    }
+
+    start.elapsed()
+}
+
+fn thpt(duration: Duration) -> f64 {
+    (BYTES_LEN as f64 / 1000000.0) / duration.as_secs_f64()
+}
